3c8cd9ff384608bf0f7929884f6dd8109cc9667d
Jiarui Wang <wangjiarui4@outlook.com>
2024-03-11 14:22:58 +0800

    feat: add scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,4 @@
-podman run -v $(pwd):/src --privileged -it localhost/cross-arm:latest sh scripts/cross-x86-aarch.sh
+#!/usr/bin/env bash
+podman run -v $(pwd):/src --privileged -it localhost/cross-arm:latest scripts/cross.sh scripts/rp3.env
+podman run -v $(pwd):/src --privileged -it localhost/cross-arm:latest scripts/cross.sh scripts/rp4.env
+podman run -v $(pwd):/src --privileged -it localhost/cross-arm:latest scripts/cross.sh scripts/aarch64.env
--- scripts/cross-x86-aarch.sh
+++
@@ -1,9 +0,0 @@
-export CC=aarch64-linux-gnu-gcc
-
-# use mold (2m2s)
-# cmake -B build -DWITH_MOLD=1 -DARM_DYNAREC=1
-# cd build; mold -run make -j; cd ..
-
-# use gnu ld (2m4s)
-cmake -B build -DARM_DYNAREC=1
-cd build; make -j; cd ..
